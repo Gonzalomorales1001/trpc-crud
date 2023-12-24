e151f03860927c10c989bcd7c316e10588e6487f
Gonzalo Morales <gonzalomorales1001@gmail.com>
2023-12-23 22:59:35 -0300

    changed backend url

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -10,7 +10,7 @@ const App = () => {
     trpc.createClient({
       links: [
         httpBatchLink({
-          url: `${import.meta.env.VITE_SERVER_URL}`
+          url: `${window.location.origin}/trpc`
         })
       ]
     })
